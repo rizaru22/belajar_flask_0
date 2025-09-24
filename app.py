754bf55b2828a6c38b06f9747112bbb1d30ecb84
rizaru22 <rizaru.2.2@gmail.com>
2025-09-24 14:02:54 +0700

    Belajar Flask Hari Pertama

---
+++ app.py
@@ -0,0 +1,18 @@
+from flask import Flask,render_template
+
+app=Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html',title="Halaman Index")
+
+@app.route('/about')
+def about():
+    return "Ini adalah halaman about"
+
+@app.route('/halo/<name>')
+def halo(name):
+    return f"Halo <strong> {name}</strong> !! Apa Kabar???"
+
+if __name__ == '__main__':
+    app.run(debug=True) 
