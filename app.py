4aa87b5100f8b813490661311d4b4d4514c5e14a
rizaru22 <rizaru.2.2@gmail.com>
2025-09-26 11:41:22 +0700

    menambahkan menu

--- app.py
+++ app.py
@@ -2,17 +2,27 @@ from flask import Flask,render_template
 
 app=Flask(__name__)
 
+data={
+    "nama":"Muhammad Rizky",
+    "alamat":"Cilacap",
+    "jenis_kelamin":"Laki-laki",
+    "pekerjaan":"Mahasiswa",
+    "tempat_lahir":"Cilacap",
+    "tanggal_lahir":"2003-01-01",
+    "foto":"senyum.png"
+}
+
 @app.route('/')
 def index():
-    return render_template('index.html',title="Halaman Index")
+    return render_template('index.html',title="Halaman Index",isi="Selamat Datang di Website Flask")
 
 @app.route('/about')
 def about():
-    return "Ini adalah halaman about"
+   return render_template('about.html',title="Halaman About",isi="Ini Halaman About")
 
-@app.route('/halo/<name>')
-def halo(name):
-    return f"Halo <strong> {name}</strong> !! Apa Kabar???"
+@app.route('/biodata')
+def halo():
+    return render_template('biodata.html',title="Biodata",isi=data)
 
 if __name__ == '__main__':
     app.run(debug=True) 
